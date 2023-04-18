fd5648ec8341e1e330d04d4f8ca437e2f10b804a
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-04-18 16:36:32 -0600

    now displaying movie names on movie screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
+import Header from "./components/Header";
+import MovieScreen from "./components/MovieScreen";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
-      
+      <Header />
+      <main>
+        <MovieScreen
+          movieList={movieList}
+          page={page}
+          setPage={setPage}
+          watchList={watchlist}
+        />
+      </main>
     </div>
   );
 }
